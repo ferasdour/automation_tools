97145d6e1bbcb28d2250795f7c39e6adaa52982d
FeemcoTech <info@feemcotech.solutions>
2024-09-11 04:00:02 -0500

    daily push; for fun and profit

--- sanitize.sh
+++ sanitize.sh
@@ -3,3 +3,5 @@
 #printf "examples:\npapermill initial-scans.ipynb /dev/stdout -p input_data feemcotech.solutions\npapermill libvirt-provision.ipynb /dev/stdout -p hostname ubuntu -p domainname private -p url https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img -p os_variant ubuntu-stable-latest -p size 80G -p ram 4096\n"
 jupyter nbconvert --clear-output --to notebook --output=/share/public-git-repos/automation-tools/initial-scans.ipynb /share/pentests/ansible-pentest/initial\ scans.ipynb >/dev/null
 jupyter nbconvert --clear-output --to notebook --output=/share/public-git-repos/automation-tools/libvirt-provision.ipynb /share/pentests/ansible-pentest/libvirt-provision.ipynb >/dev/null
+cp /share/pentests/ansible-pentest/buildContainerFromVM.yml /share/public-git-repos/automation-tools/buildContainerFromVM.yml
+cp /share/pentests/ansible-pentest/k3d-provision.yml /share/public-git-repos/automation-tools/k3d-provision.yml
