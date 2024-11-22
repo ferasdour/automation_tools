2436acc1536a7ed5208909846f24f813a33be377
FeemcoTech <info@feemcotech.solutions>
2024-11-22 13:46:38 -0600

    daily push; for fun and profit

--- sanitize.sh
+++ sanitize.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # grab new versions of the scripts, sanitize, then add to git
-#printf "examples:\npapermill initial-scans.ipynb /dev/stdout -p input_data feemcotech.solutions\npapermill libvirt-provision.ipynb /dev/stdout -p hostname ubuntu -p domainname private -p url https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img -p os_variant ubuntu-stable-latest -p size 80G -p ram 4096\n"
-jupyter nbconvert --clear-output --to notebook --output=/share/public-git-repos/automation-tools/initial-scans.ipynb /share/pentests/ansible-pentest/initial\ scans.ipynb >/dev/null
-jupyter nbconvert --clear-output --to notebook --output=/share/public-git-repos/automation-tools/libvirt-provision.ipynb /share/pentests/ansible-pentest/libvirt-provision.ipynb >/dev/null
-cp /share/pentests/ansible-pentest/buildContainerFromVM.yml /share/public-git-repos/automation-tools/buildContainerFromVM.yml
-cp /share/pentests/ansible-pentest/k3d-provision.yml /share/public-git-repos/automation-tools/k3d-provision.yml
+# printf "examples:\npapermill initial-scans.ipynb /dev/stdout -p input_data feemcotech.solutions\npapermill libvirt-provision.ipynb /dev/stdout -p hostname ubuntu -p domainname private -p url https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img -p os_variant ubuntu-stable-latest -p size 80G -p ram 4096\n"
+jupyter nbconvert --clear-output --to notebook --output=/share/git-repo/Scripts/automation-tools/initial-scans.ipynb /share/git-repo/Notebooks/initial\ scans.ipynb >/dev/null
+jupyter nbconvert --clear-output --to notebook --output=/share/git-repo/Scripts/automation-tools/libvirt-provision.ipynb /share/git-repo/Notebooks/libvirt-provision.ipynb >/dev/null
+
